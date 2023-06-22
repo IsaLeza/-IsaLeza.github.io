5ba77b9e461cb0bad69609e43d832e1befcce802
Isaias Lezama Aponte <isaiaslezama@gmail.com>
2023-06-21 22:59:21 -0600

    portfolio

--- gato.js
+++ gato.js
@@ -1,62 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cells = document.querySelectorAll('.cell');
-    let currentPlayer = 'X';
-  
-    cells.forEach(function (cell) {
-      cell.addEventListener('click', handleCellClick);
+document.addEventListener("DOMContentLoaded", function() {
+  const slider = document.querySelector(".project-slider");
+  const slides = slider.querySelectorAll(".project-slide");
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.style.display = "block";
+      } else {
+        slide.style.display = "none";
+      }
     });
-  
-    function handleCellClick() {
-      if (this.textContent !== '') return;
-      this.textContent = currentPlayer;
-      this.classList.add(currentPlayer);
-  
-      if (checkWin(currentPlayer)) {
-        alert('Player ' + currentPlayer + ' wins!');
-        resetGame();
-        return;
-      }
-  
-      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-  
-      if (checkDraw()) {
-        alert('It\'s a draw!');
-        resetGame();
-        return;
-      }
+  }
+
+  function navigateNext() {
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+      currentIndex = 0;
     }
-  
-    function checkWin(player) {
-      const winningCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-      ];
-  
-      return winningCombinations.some(combination => {
-        return combination.every(index => {
-          return cells[index].textContent === player;
-        });
-      });
+    showSlide(currentIndex);
+  }
+
+  function navigatePrev() {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = slides.length - 1;
     }
-  
-    function checkDraw() {
-      return [...cells].every(cell => {
-        return cell.textContent !== '';
-      });
-    }
-  
-    function resetGame() {
-      cells.forEach(cell => {
-        cell.textContent = '';
-        cell.classList.remove('X', 'O');
-      });
-      currentPlayer = 'X';
-    }
-  });
-  
+    showSlide(currentIndex);
+  }
+
+  prevButton.addEventListener("click", navigatePrev);
+  nextButton.addEventListener("click", navigateNext);
+
+  showSlide(currentIndex);
+});
